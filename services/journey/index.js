8e8cf391355e11826ccd4d7b360a96fbdbf488ff
Yu Hao Shao <yuhaoshao@Yus-MacBook-Air.local>
2025-03-28 14:46:22 -0700

    changes made

--- services/journey/index.js
+++ services/journey/index.js
@@ -1,6 +1,6 @@
 const getJourneys = require('./get_journeys').default;
 const createJoruney = require('./create_journey').default;
-const updateJourney = require('./update_journey').default;
+const updateJourney = require('./update_journey').default
 
 module.exports={
     getJourneys,

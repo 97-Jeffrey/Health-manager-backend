a3e11787406f43bfd729df63874b4f8cf6ca577b
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2025-04-29 11:42:06 -0700

    added new routes for fitness

--- routes/fitness.js
+++ routes/fitness.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const awsMealService = require('../services/meal')
-const awsRecipeService = require('../services/recipe')
+const awsFitnessService = require('../services/fitness')
 const { validateUser, printInfo, printError} = require('../helpers/general_helpers')
 const { v4: uuidv4 } = require('uuid')
 
@@ -9,7 +8,7 @@ const NAMESPACE = 'FITNESS';
 
 
 
-// get all meals
+// get all sports entry
 router.get('/:userId/sports', async(req, res)=>{
 
     const { userId } = req.params;
@@ -23,25 +22,25 @@ router.get('/:userId/sports', async(req, res)=>{
     }
 
     try{
-        const { Item } = await awsMealService.getMeals(userId)
-        const mealData= Item?.meal?? []
+        const { Item } = await awsFitnessService.getFitness(userId)
+        const sportData= Item?.sport?? []
 
         console.log('Item', Item)
 
-        res.status(200).send(mealData)
+        res.status(200).send(sportData)
        
         
     }catch(err){
-        printError('route', NAMESPACE, 'getMeals', err)
+        printError('route', NAMESPACE, 'getFitness', err)
         res.status(400).send(
-            `Route error: path="/${NAMESPACE}/:userId/meals" method="GET".`
+            `Route error: path="/${NAMESPACE}/:userId/sports" method="GET".`
         )
         throw err
     }
 })
 
 
-// create a brand new meal
+// create a brand new sport
 router.post('/:userId/create', async (req, res) => {
     try {
         const { userId } = req.params
@@ -51,24 +50,21 @@ router.post('/:userId/create', async (req, res) => {
             )
             return
         }
-        const mealId = uuidv4();
-        const { meal } = req.body;
-        const newMeal = {...meal, id: mealId }
+        const sportId = uuidv4();
+        const { sport } = req.body;
+        const newSport = {...sport, id: sportId }
 
         try{
-
-            const micronutrients = await awsRecipeService.parseNutrients(newMeal.name)
-            printInfo('awsRecipeService', NAMESPACE,'parseNutrients',{})
   
 
-            const data = await awsMealService.createMeal(userId, {...newMeal, micronutrients})
-            printInfo('awsMealService', NAMESPACE,'createMeal',data.Attributes)
+            const data = await awsFitnessService.createFitness(userId, 'sport', newSport)
+            printInfo('awsFitnessService', NAMESPACE,'createFitness',data.Attributes)
             
-            res.status(200).send('Meal Create Success')
+            res.status(200).send('Sport Create Success')
                 
         }
         catch(err){
-            printError('awsMealService', NAMESPACE, 'createMeal', err)
+            printError('awsFitnessService', NAMESPACE, 'createFitness', err)
             res.status(400).send(
                 `Service error: path="/${NAMESPACE}/:userId/create" method="POST".`
             )
@@ -76,7 +72,7 @@ router.post('/:userId/create', async (req, res) => {
         }
         
     } catch (err) {
-        printError('awsJourneyService', NAMESPACE, 'createJourney', err)
+        printError('awsFitnessService', NAMESPACE, 'createFitness', err)
         res.status(400).send(
             `Route error: path="/${NAMESPACE}/:userId/create" method="POST".`
         )
@@ -85,41 +81,37 @@ router.post('/:userId/create', async (req, res) => {
 })
 
 
-// update a existing meal
-router.put(`/:userId/edit/:mealId`, async (req, res) => {
+// update a existing sport
+router.put(`/:userId/edit/:sportId`, async (req, res) => {
 
-    const { userId, mealId } = req.params
+    const { userId, sportId } = req.params
     if (!validateUser(req)) {
         res.status(401).send(
-            `Unauthorized request: path="/${NAMESPACE}/:userId/edit/:mealId" method="PUT".`
+            `Unauthorized request: path="/${NAMESPACE}/:userId/edit/:sportId" method="PUT".`
         )
         return
     }
-    const { meal } =req.body
+    const { sport } =req.body
 
 
     try {
 
-
-        const micronutrients = await awsRecipeService.parseNutrients(meal.name)
-        printInfo('awsRecipeService', NAMESPACE,'parseNutrients',{})
-
-        const meals = await awsMealService.getMeals(userId)
-        const newMeals =meals.Item.meal.map(eachMeal => eachMeal.id !== mealId? eachMeal: {...meal, micronutrients});
+        const sports = await awsFitnessService.getFitness(userId)
+        const newSports =sports.Item.sport.map(spo => spo.id !== sportId? spo: sport);
 
 
-        const data = await awsMealService.updateMeal(userId, newMeals);
-        printInfo('awsMealService', NAMESPACE,'updateMeal', data.Attributes)
+        const data = await awsFitnessService.updateFitness(userId, 'sport', newSports);
+        printInfo('awsFitnessService', NAMESPACE,'updateFitness', data.Attributes)
 
-        res.status(200).send('Meal Update Success')
+        res.status(200).send('Sport Update Success')
 
             
 
     } catch (err) {
    
-        printError('awsMealService', NAMESPACE, 'updateMeal', err)
+        printError('awsFitnessService', NAMESPACE, 'updateFitness', err)
         res.status(400).send(
-            `Service error: path="/${NAMESPACE}/${userId}/edit/:mealId" method="PUT".`
+            `Service error: path="/${NAMESPACE}/${userId}/edit/:sportId" method="PUT".`
         )
         
     }
@@ -128,7 +120,7 @@ router.put(`/:userId/edit/:mealId`, async (req, res) => {
 
 
 
-// Delete A Meal:
+// Delete A fitness:
 router.post(`/:userId/delete`, async (req, res) => {
 
     const { userId } = req.params
@@ -138,20 +130,20 @@ router.post(`/:userId/delete`, async (req, res) => {
         )
         return
     }
-    const { mealId } = req.body;
+    const { sportId } = req.body;
 
     try {
-        const meals = await awsMealService.getMeals(userId)
-        const newMeals = [...meals.Item.meal.filter(meal => meal.id !== mealId)];
+        const sports = await awsFitnessService.getFitness(userId)
+        const newSports = [...sports.Item.sport.filter(sport => sport.id !== sportId)];
     
 
-        await awsMealService.updateMeal(userId, newMeals)
+        await awsFitnessService.updateFitness(userId, "sport", newSports)
         
-        res.status(200).send('Meal Delete Success')
+        res.status(200).send('fitness Delete Success')
            
 
     } catch (err) {
-        printError('awsMealService', NAMESPACE, 'deleteMeal', err)
+        printError('awsFitnessService', NAMESPACE, 'deleteFitness', err)
         res.status(400).send(
             `Service error: path="/${NAMESPACE}/${userId}/delete" method="POST".`
         )
